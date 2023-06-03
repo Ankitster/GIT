250eec52669990dd2fe3f134084031a39d1bd21c
Ankit Bhardwaj <skygamer804@gmail.com>
2023-06-03 16:00:49 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -4,6 +4,6 @@ public static void main(String args[])
 {  
 int a = 5, b = 6, sum;  
 sum = a + b;  
-System.out.println("The sum of numbers is: "+sum);  
+System.out.println("The sum of given two numbers is: "+sum);  
 }  
 }  
