825e46eff0e552d13e32ac928198f2f873e57e83
Ankit Bhardwaj <skygamer804@gmail.com>
2023-06-09 11:22:16 +0530

    added multiply feature

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static void main(String args[])   
+{  
+int a = 5, b = 6, prod;  
+prod = a*b;  
+System.out.println("The product of given two numbers is: "+prod);  
+}  
+}
